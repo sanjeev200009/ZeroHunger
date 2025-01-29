eb8057eb2e4f7f3f37fd3fb341f1aabdd7271383
user <sanjaysanjeev2000@gmail.com>
2025-01-30 01:26:04 +0530

    add new slider feature for the home page

--- app/src/main/java/com/example/zerohunger/featuredpartnersActivity.java
+++ app/src/main/java/com/example/zerohunger/featuredpartnersActivity.java
@@ -7,24 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
 
-public class Activityhome extends AppCompatActivity {
+public class featuredpartnersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_activityhome);
+        setContentView(R.layout.activity_featuredpartners);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Set up the image slider (ViewPager2)
-        int[] images = {R.drawable.homeimg, R.drawable.homeimg, R.drawable.homeimg};  // Replace with your drawable images
-        ViewPager2 viewPager = findViewById(R.id.viewPager);
-        ImageSliderAdapter adapter = new ImageSliderAdapter(this, images);
-        viewPager.setAdapter(adapter);
     }
 }
