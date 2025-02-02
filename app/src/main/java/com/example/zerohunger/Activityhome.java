dc6b823f1981d76382e19cbb1eff5d9e2ec94ce9
user <sanjaysanjeev2000@gmail.com>
2025-02-03 01:34:25 +0530

    Finished home page

--- app/src/main/java/com/example/zerohunger/Activityhome.java
+++ app/src/main/java/com/example/zerohunger/Activityhome.java
@@ -1,7 +1,8 @@
 package com.example.zerohunger;
 
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.os.Looper;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,22 +10,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Activityhome extends AppCompatActivity {
 
+    private ViewPager2 viewPager;
+    private final Handler sliderHandler = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_activityhome);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Set up the image slider (ViewPager2)
-        int[] images = {R.drawable.homeimg, R.drawable.homeimg, R.drawable.homeimg};  // Replace with your drawable images
-        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        int[] images = {R.drawable.ban1, R.drawable.ban3, R.drawable.ban2};
+        viewPager = findViewById(R.id.viewPager);
         ImageSliderAdapter adapter = new ImageSliderAdapter(this, images);
         viewPager.setAdapter(adapter);
+        startAutoSlide();
+
     }
+
+    private void startAutoSlide() {
+        sliderHandler.postDelayed(sliderRunnable, 3000);
+    }
+
+    private final Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            int currentItem = viewPager.getCurrentItem();
+            int nextItem = (currentItem + 1) % 3;
+            viewPager.setCurrentItem(nextItem, true);
+            sliderHandler.postDelayed(this, 3000);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+
 }
